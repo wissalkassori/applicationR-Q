28a75f9bb6bde49eef1c4f6431ee969ac3b7ab9c
wissalkassori <kassoriwissal11@gmail.com>
2025-04-18 19:39:19 +0100

    Update HibernateUtil.java

--- Webapp/src/java/util/HibernateUtil.java
+++ Webapp/src/java/util/HibernateUtil.java
@@ -1,35 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.SessionFactory;
-
 /**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
  *
- * @author Hp
+ * @author ichou
  */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
-    
+
     static {
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure("config/hibernate.cfg.xml").buildSessionFactory();
+            Configuration configuration = new Configuration().configure("config/hibernate.cfg.xml");
+
+            configuration.addAnnotatedClass(entities.Categorie.class);
+            configuration.addAnnotatedClass(entities.Question.class);
+            configuration.addAnnotatedClass(entities.Reponse.class);
+            configuration.addAnnotatedClass(entities.User.class);
+            configuration.addAnnotatedClass(entities.Admin.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Échec de la création de la SessionFactory : " + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
